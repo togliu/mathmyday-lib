610de2bda77982183c083984b2a30baf297f698d
Lars Tennstedt <27898583+ltennstedt@users.noreply.github.com>
2021-06-17 20:26:12 +0200

    Property Testing
    Edge cases excluded

--- finnmath/src/test/kotlin/io/github/ltennstedt/finnmath/extension/IntsSpec.kt
+++ finnmath/src/test/kotlin/io/github/ltennstedt/finnmath/extension/IntsSpec.kt
@@ -18,35 +18,56 @@ package io.github.ltennstedt.finnmath.extension
 
 import com.google.common.math.IntMath
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
-import io.kotest.property.Arb
-import io.kotest.property.arbitrary.int
+import io.kotest.property.PropertyTesting
+import io.kotest.property.RandomSource
+import io.kotest.property.arbitrary.arbitrary
+import io.kotest.property.arbitrary.filter
 import io.kotest.property.forAll
 import kotlin.math.absoluteValue
+import kotlin.random.nextInt
 
-val intArb = Arb.int((-10)..10)
+val arbOdd = arbitrary { rs: RandomSource ->
+    rs.random.nextInt((-10)..10)
+}.filter { it % 2 == 1 }
+
+val arbEven = arbitrary { rs: RandomSource ->
+    rs.random.nextInt((-10)..10)
+}.filter { it % 2 == 0 }
+
+val arbInt = arbitrary { rs: RandomSource ->
+    rs.random.nextInt((-10)..10)
+}
 
 @Suppress("UNUSED")
 object IntExtensionsSpec : FunSpec({
+    PropertyTesting.defaultIterationCount = 10
     context("isEven") {
         test("should return false when an Int is odd") {
-            1.isEven() shouldBe false
+            forAll(arbOdd) { a ->
+                !a.isEven()
+            }
         }
         test("should return true when an Int is even") {
-            2.isEven() shouldBe true
+            forAll(arbEven) { a ->
+                a.isEven()
+            }
         }
     }
     context("isOdd") {
         test("should return false when an Int is even") {
-            2.isOdd() shouldBe false
+            forAll(arbEven) { a ->
+                !a.isOdd()
+            }
         }
         test("should return true when an Int is odd") {
-            1.isOdd() shouldBe true
+            forAll(arbOdd) { a ->
+                a.isOdd()
+            }
         }
     }
     context("gcd") {
         test("should call IntMath.gcd with absolute values") {
-            forAll(intArb, intArb) { a, b ->
+            forAll(arbInt, arbInt) { a, b ->
                 a.gcd(b) == IntMath.gcd(a.absoluteValue, b.absoluteValue)
             }
         }
